f3b27b21fd7944f0f293573432fa26171d19cea7
Ousman Touray <otouray14@gmail.com>
2025-05-29 20:36:57 -0400

    setup a simple helper function, need to flush out

--- src/tracker.c
+++ src/tracker.c
@@ -1,1 +1,17 @@
 #include "tracker.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void add_meal() {
+  Meal ate;
+  printf("What did you just eat?\n");
+  scanf("%s", ate.meal);
+  printf("How many calories was your meal?\n");
+  scanf("%d", &ate.calories);
+  printf("How much protein was in your meal?\n");
+  scanf("%d", &ate.protein);
+  printf("How much carbs was in your meal?\n");
+  scanf("%d", &ate.carbs);
+  printf("How much fat was in your meal?\n");
+  scanf("%d", &ate.fats);
+}
